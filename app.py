8d4c0def3d34f8e6eb413ac2317a30c55a0e03eb
izdong <116863850+izdong@users.noreply.github.com>
2022-11-16 17:46:17 +0700

    header hidden

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 
 # ---------------------------------pageconfig---------------------------------
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 st.set_page_config(
                     layout="wide",
                     page_icon="🌲",
