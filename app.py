08415133b9c018c030303277df59744e887d2e5f
izdong <116863850+izdong@users.noreply.github.com>
2022-11-16 17:52:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,15 @@ st.set_page_config(
                     menu_items = None
                     )
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 #get and cache data
 # @st.experimental_memo
 # def get_data():
