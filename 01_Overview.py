a35aa1a51b7164d22367424aec76813ccb48a72a
izdong <116863850+izdong@users.noreply.github.com>
2022-12-12 13:25:12 +0700

    Update 01_Overview.py

--- 01_Overview.py
+++ 01_Overview.py
@@ -12,16 +12,16 @@ from modules.importer import DataImport
 
 
 # Title page and footer
-title = "TEST69"
+title = "Overview"
 t = Title().page_config(title)
 f = Footer()
-
 
 # Import data
 df = DataImport().fetch_and_clean_data()
 df['day'] = df['date'].dt.day_name().str[:3]
 df['month'] = df['date'].dt.month_name().str[:3]
 
+# customize data
 f1 = f1_score(df['int_class'], df['precision'])
 f1_df = pd.DataFrame({'pass': [f1], 'fail': [1 - f1]}, index = ['G'])
 sort_date = df.sort_values(by='date')
@@ -50,79 +50,96 @@ t_yest = total - t_today
 t_all_delta = ((total - t_yest) * 100) / t_yest
 t_all_delta = round(t_all_delta,1)
 
-
+#  row 1
+container1 = st.container()
 col1, col2 = st.columns(2)
-with col1:
-    st.markdown("<h4 style='text-align: center;'>Accuracy</h4>", unsafe_allow_html=True)
-    pie = px.pie(f1_df.T, values = 'G', hole = .4321, names = f1_df.T.index)
-    pie.update_layout(height=250) 
-    st.plotly_chart(pie, use_container_width=True,height=250)
-    st.write(
-    """
-    <style>
-    [data-testid="stMetricDelta"] svg {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-    ) # hide up arrow st.mertric{delta}
-    st.metric('Total',total,f'{t_all_delta}%')
-    f12 = (f1 * 100)
-    st.metric('correct (%)',round(f12, 2))
-    st.metric('incorrect (%)',round((100-f12),2))
 
-    # center style
-    st.markdown('''
-    <style>
-    /*center metric label*/
-    [data-testid="stMetricLabel"] > div:nth-child(1) {
-        justify-content: center;
-    }
+# column in row 1
+with container1:
+    with col1:
+        st.markdown("<h4 style='text-align: center;'>Accuracy</h4>", unsafe_allow_html=True)
+        accuracy = px.pie(f1_df.T, values = 'G', hole = .4321, names = f1_df.T.index)
+        accuracy
 
-    /*center metric value*/
-    [data-testid="stMetricValue"] > div:nth-child(1) {
-        justify-content: center;
-    }
-
-    /*center metric value*/
-    [data-testid="stMetricDelta"] > div:nth-child(2) {
-        justify-content: center;
-    }
-    </style>
-    ''', unsafe_allow_html=True)
-
-with col2:
-    container1 = st.container()
-    with container1:
+    with col2:
         st.markdown("<h4 style='text-align: center;'>Transaction</h4>", unsafe_allow_html=True)
 
         tab1, tab2, tab3 = st.tabs(["Overall", "Month", "Day of Week"])
         with tab1:
             overall = px.histogram(sort_date, x = 'date', text_auto = True)
-            overall.update_layout(bargap = 0.15, height=300)
-            st.plotly_chart(overall, use_container_width=True,height=300)
+            overall.update_layout(bargap = 0.15)
+            overall
 
         with tab2:
             month = px.histogram(sort_date, x = 'month', text_auto = True)
-            month.update_layout(bargap=0.2, height=300)
-            st.plotly_chart(month, use_container_width=True,height=300)
+            month.update_layout(bargap=0.2)
+            month
 
         with tab3:
             dow = px.histogram(df, x="day", category_orders=dict(day=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]), text_auto = True)
-            dow.update_layout(height=300) 
-            st.plotly_chart(dow, use_container_width=True,height=300)
-    container2 = st.container()
-    with container2:
+            dow
+
+#  row 2
+
+container2 = st.container()
+col3, col4 = st.columns(2)
+
+#  columns in row 2
+with container2:
+    with col3:
+        st.write(
+        """
+        <style>
+        [data-testid="stMetricDelta"] svg {
+            display: none;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+        )
+
+        st.metric('Total',total,f'{t_all_delta}%')
+        f12 = (f1 * 100)
+        st.metric('correct (%)',round(f12, 2))
+        st.metric('incorrect (%)',round((100-f12),2))
+
+        # center style
+        st.markdown('''
+        <style>
+        /*center metric label*/
+        [data-testid="stMetricLabel"] > div:nth-child(1) {
+            justify-content: center;
+        }
+
+        /*center metric value*/
+        [data-testid="stMetricValue"] > div:nth-child(1) {
+            justify-content: center;
+        }
+
+        /*center metric value*/
+        [data-testid="stMetricDelta"] > div:nth-child(2) {
+            justify-content: center;
+        }
+        </style>
+        ''', unsafe_allow_html=True)
+
+
+    with col4:              #right
+        # st.markdown('% Type of Wood')
+        # st.markdown("<h4 style='text-align: center;'>% Type of Wood</h>", unsafe_allow_html=True)
         st.markdown("<h4 style='text-align: center;'>Wood Type</h4>", unsafe_allow_html=True)
         tab1, tab2= st.tabs(["Percent", "Amount"])
+        
+        
 
         with tab1:
             Type = px.histogram(df, x = 'class', histnorm = 'percent', text_auto = True)
-            Type.update_layout(height=300)
-            st.plotly_chart(Type, use_container_width=True,height=300)
+            Type
             
         with tab2:
             Type = px.histogram(df, x = 'class', histnorm = 'density', text_auto = True)
-            Type.update_layout(height=300)
-            st.plotly_chart(Type, use_container_width=True,height=300)
+            Type
+
+
+container3 = st.container()
+col1, col2 = st.columns(2)
